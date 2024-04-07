a902f3dc927c6f47cb87f886375916ecec835d28
DimaS <dimitrysavo@gmail.com>
2024-04-07 19:17:28 +0300

    Add additional message details

--- Bottle_5_6/myForm.py
+++ Bottle_5_6/myForm.py
@@ -1,5 +1,6 @@
-from bottle import post, request
+from bottle import post, request, response
 import re
+from datetime import date
 
 def is_valid_email(email):
     pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
@@ -8,7 +9,9 @@ def is_valid_email(email):
 @post('/home', method='post')
 def my_form():
     mail = request.forms.get('ADRESS')
+    name = request.forms.get('USERNAME')
+    today = date.today()
     if(is_valid_email(mail)):
-        return f"Thanks! The andwer will be sent to the mail {mail}"
+        return f"Thanks, {name}! The answer will be sent to the mail {mail}. Access date: {today}"
     else:
         return "Invalid email adress"
