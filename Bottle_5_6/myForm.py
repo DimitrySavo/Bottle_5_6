70a2e65873402feaf2b94862c3ccf977844a6e62
DimaS <dimitrysavo@gmail.com>
2024-04-07 18:21:04 +0300

    Add validation for mail address

--- Bottle_5_6/myForm.py
+++ Bottle_5_6/myForm.py
@@ -1,6 +1,14 @@
 from bottle import post, request
+import re
+
+def is_valid_email(email):
+    pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+    return bool(re.match(pattern, email))
 
 @post('/home', method='post')
 def my_form():
-    mail = request.forms.get('ADDRESS')
-    return f"Thanks! The andwer will be sent to the mail {mail}"
+    mail = request.forms.get('ADRESS')
+    if(is_valid_email(mail)):
+        return f"Thanks! The andwer will be sent to the mail {mail}"
+    else:
+        return "Invalid email adress"
