8198b66bc55c973de0b3ba3fd9a904bbc7b9a452
DimitryS <dimitrysavo@gmail.com>
2024-04-14 15:49:46 +0300

    Add questions validation. Now questions writiong into JSON

--- Bottle_5_6/myForm.py
+++ Bottle_5_6/myForm.py
@@ -1,6 +1,8 @@
 from bottle import post, request, response
 import re
+import json
 import pdb
+import os
 from datetime import date
 
 def is_valid_email(email):
@@ -9,21 +11,29 @@ def is_valid_email(email):
 
 @post('/home', method='post')
 def my_form():
-    questions = {}
-
-    pdb.set_trace()
+    if os.path.getsize('jsonQuestions.json') > 0:
+        questions = json.load(open('jsonQuestions.json', 'r'))
+    else:
+        questions = {}
 
     question = request.forms.get('QUEST')
     mail = request.forms.get('ADRESS')
     name = request.forms.get('USERNAME')
     today = date.today()
-    if(name == "" or mail == "" or questions == ""):
+    if(name == "" or mail == "" or question == ""):
         return "Please, fill all the fields"
+    elif(any(char.isdigit() for char in question) or len(question) < 3):
+        return "Invalid question"
     elif(not is_valid_email(mail)):
         return "Invalid email adress"
     else:
         if mail in questions:
-            questions[mail].append(question)
+            if question in questions[mail]:
+                return "This question was already asked"
+            else:
+                questions[mail].append(question)
+                json.dump(questions, open('jsonQuestions.json', 'w'))
         else:
             questions[mail] = [question]
+            json.dump(questions, open('jsonQuestions.json', 'w'))
         return f"Thanks, {name}! The answer will be sent to the mail {mail}. Access date: {today}"
