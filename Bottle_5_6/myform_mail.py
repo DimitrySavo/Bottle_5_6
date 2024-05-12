24910842f764c4a062694a04ad343f6c6735e352
DimitryS <dimitrysavo@gmail.com>
2024-05-12 14:52:58 +0300

    add mail pattern

--- Bottle_5_6/myform_mail.py
+++ Bottle_5_6/myform_mail.py
@@ -1,6 +1,6 @@
 import re
 
-pattern = r"^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$"
+pattern = r"^[a-zA-Z0-9]{1}[a-zA-Z0-9_.+,*%$#!-]{0,254}@[a-zA-Z0-9-]{1,64}(\.[a-z0-9-]{2,7})+$" 
 
 def check_mail(mail):
     return bool(re.match(pattern, mail))
--- Bottle_5_6/test_mail.py
+++ Bottle_5_6/test_mail.py
@@ -3,7 +3,11 @@ import unittest
 
 mails_correct = ["test@mail.ru",
                  "dima@st.guap.ru",
-                 "Emil@gmail.com"]
+                 "Emil@gmail.com",
+                 "123@gmail.com",
+                 "2___@gmail.com",
+                 "2...@gmail.com",
+                 "f@gmail.com"]
 mails_uncorrect = ["",
                    " ",
                    "123123",
@@ -12,7 +16,12 @@ mails_uncorrect = ["",
                    "@mail.com",
                    "dwqd @ sss.sd",
                    "@.ru",
-                   "asdsd@@@@@mail.com"]
+                   "asdsd@@@@@mail.com",
+                   ("s"*300) + "@gmail.com",
+                   "s@" + ("g" * 65) + ".com",
+                   "dima@gmail." + ("d" * 10),
+                   "dima@gmail.r",
+                   "dima@gmail..com"]
 
 class MailTest(unittest.TestCase):
     def test_T_mail(self):
@@ -23,5 +32,5 @@ class MailTest(unittest.TestCase):
         for mail in mails_uncorrect:
             self.assertFalse(myform_mail.check_mail(mail))
             
-if name == "main":
+if __name__ == "__main__":
     unittest.main()
